dcd6c1280d6dc1700b6831e9f68ca4785b0b405d
stevey d <stphndcky22@gmail.com>
2022-02-20 14:53:54 -0800

    added prompts asking what user would like password to contain, restrictions on what user can have as password

--- script.js
+++ script.js
@@ -20,11 +20,14 @@ function generatePassword() {
 
     // define special characters
     // special characters obtained from: https://owasp.org/www-community/password-special-characters
-    const special = " !#$%&'()*+,-./:;<=>?@[\]^_`{}|~"
+    const special = " !#$%&'()*+,-./:;<=>?@[}^_`{}|~"
 
     // i am using let instead of var because let cannot be redeclared
     // prompt for password length
     let passwordLength = prompt("Password may contain 8 to 128 characters. How many characters?");
+    // passwordLength is entered as string, we want an integer so...
+    passwordLength = parseInt(passwordLength);
+
     // prompt for lowercase
     let passwordLowercase = confirm("Would you like your password to contain lowercase letters?");
     // prompt for uppercase
@@ -33,7 +36,53 @@ function generatePassword() {
     let passwordNumbers = confirm("Would you like your password to contain numbers?");
     // prompt for special characters
     let passwordSpecial = confirm("Would you like your password to contain special characters?");
-    
+
+
+    // password will add on each group as it is confirmed, so let's define password as ""
+    let password = "";
+    let randomPassword = "";
+
+    // now we add on each group
+    if (passwordLowercase === true) {
+        randomPassword = randomPassword + lowercase;
+    }
+
+    if (passwordUppercase === true) {
+        randomPassword = randomPassword + uppercase;
+    }
+
+    if (passwordNumbers === true) {
+        randomPassword = randomPassword + numeric;
+    }
+
+    if (passwordSpecial === true) {
+        randomPassword = randomPassword + special;
+    }
+
+    // let's make sure the password length stays between 8 and 128
+    if (passwordLength >= 8 && passwordLength <= 128) {
+        for (let i = 0; i < userLength; i++) {    
+            let specialCharacter = randomPassword[Math.floor(Math.random()*randomPassword.length)];
+            password = password + specialCharacter;
+        }
+    }
+    // if the length doesnt meet requirement
+    else {
+        window.alert("Password must be between 8 and 128 characters.");
+    }
+
+    // what is user inputs nothing?
+    if (randomPassword === "") {
+        window.alert("Password must be between 8 and 128 characters.");
+        // reset password
+        password = "";
+    }
+
+
+
+    return password;
+
+
 }
 
 
