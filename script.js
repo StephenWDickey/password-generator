c71f38330545e686a73abd152ab95df629d2842f
stevey d <stphndcky22@gmail.com>
2022-02-20 14:20:42 -0800

    added criteria for lowercase, uppercase, numbers, and special characters, started generatePassword function

--- script.js
+++ script.js
@@ -3,8 +3,47 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+////////////////////////////////////////////////////////
 
+// i will write the generatePassword function here
+function generatePassword() {
+
+    // i am using const instead of var because const values cannot be reassigned 
+    // define lowercase
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+    // define uppercase
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // define numbers
+    const numeric = "0123456789";
+
+    // define special characters
+    // special characters obtained from: https://owasp.org/www-community/password-special-characters
+    const special = " !#$%&'()*+,-./:;<=>?@[\]^_`{}|~"
+
+    // i am using let instead of var because let cannot be redeclared
+    // prompt for password length
+    let passwordLength = prompt("Password may contain 8 to 128 characters. How many characters?");
+    // prompt for lowercase
+    let passwordLowercase = confirm("Would you like your password to contain lowercase letters?");
+    // prompt for uppercase
+    let passwordUppercase = confirm("Would you like your password to contain uppercase letters?");
+    // prompt for numbers
+    let passwordNumbers = confirm("Would you like your password to contain numbers?");
+    // prompt for special characters
+    let passwordSpecial = confirm("Would you like your password to contain special characters?");
+    
+}
+
+
+
+
+//////////////////////////////////////////////////////
 // Write password to the #password input
+// this function will make a password show up
+// we need to make the generatePassword function,
+// I will write the generate Password function above this one
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -13,5 +52,8 @@ function writePassword() {
 
 }
 
+
+///////////////////////////////////////////////////////////
 // Add event listener to generate button
+// this is will run the writePassword function when clicked
 generateBtn.addEventListener("click", writePassword);
